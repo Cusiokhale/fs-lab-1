581f9f0c826743088694ba4c60928231072b9ef8
Cordelia Usiokhale <cusiokhale@academic.rrc.ca>
2026-01-17 18:21:22 -0600

    Initialize Lab 1.2 React app

---
+++ lab-1-2/src/App.tsx
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <>
+      <header>
+        <h1>Pixell River Employee Directory</h1>
+      </header>
+
+      <main>
+        <section>
+          <h2>Employees</h2>
+          <p>Employee list will be displayed here.</p>
+        </section>
+      </main>
+
+      <footer>
+        <p>© {new Date().getFullYear()} Pixell River Financial</p>
+      </footer>
+    </>
+  );
+}
